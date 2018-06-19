950a03c7f646accc79a5539fb9b7a4ddc0343f7c
bdirito91@gmail.com <bdirito91@gmail.com>
2018-06-19 23:47:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a,b) {
+    var add = a+b;
+    return add;
+}
+function subtract (a,b) {
+  var subtract = a-b;
+  return subtract;
+}
+function multiply (a,b) {
+  var multiply = a*b;
+  return multiply;
+}
+function divide (a,b) {
+  var divide = a/b;
+  return divide;
+}
+function inc(n) {
+  var inc = a+1;
+  return inc;
+}
+function dec(n) {
+  var dec = a-1;
+  return dec;
+}
