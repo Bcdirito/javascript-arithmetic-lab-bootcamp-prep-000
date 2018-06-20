091f284a3326edf969ddd3d343443a4e9f7987fb
bdirito91@gmail.com <bdirito91@gmail.com>
2018-06-20 00:48:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function dec(a) {
 function makeInt(n) {
   return parseInt(n, 10);
 }
-function preserveDecimal (a) {
-  parseFloat(2.222);
+function preserveDecimal (n) {
+  return parseFloat(n)
 }
